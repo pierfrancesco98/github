199dce1479296562885cf4f8952ecd93d376838c
Pierfrancesco Pisani <109861869+pierfrancesco98@users.noreply.github.com>
2026-04-01 18:00:22 +0200

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-        Coded by <a href="">Pierfrancesco Pisani</a> | Challenge by
+        Coded by <a href="https://devchallenges.io/profile/c807cc56-2a43-479c-ba3b-fe6188bab257">Pierfrancesco Pisani</a> | Challenge by
       <a href="https://www.devchallenges.io?ref=challenge" target="_blank" rel="noreferrer"> devChallenges.io</a>
     </footer>
   )
